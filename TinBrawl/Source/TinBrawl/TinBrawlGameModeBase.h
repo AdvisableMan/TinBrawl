0274bdde13227cbc4db0e38f37e7ff0d8df553dd
Andrea Gandola <gandolaandrea@gmail.com>
2018-11-08 00:41:09 +0100

    [TinBrawl] Base files.

---
+++ TinBrawl/Source/TinBrawl/TinBrawlGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TinBrawlGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINBRAWL_API ATinBrawlGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
